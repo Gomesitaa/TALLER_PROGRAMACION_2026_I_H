2a452e64eb92027f51514c6f2b8d705bf9df86ed
Valery Isabella Alfonso Gómez <valeryalfonso.ig@academia.umb.edu.co>
2026-08-04 22:50:00 -0500

    Delete InvalidBookException.java

--- InvalidBookException.java
+++
@@ -1,9 +0,0 @@
-package com.umb.taller.exception;
-
-public class InvalidBookException extends LibraryException {
-
-    public InvalidBookException(String message) {
-        super(message);
-    }
-
-}
