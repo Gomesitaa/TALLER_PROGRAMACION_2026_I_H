9403e9bd7dd24fd36bbf18d81b6354ee7475d8d3
Valery Isabella Alfonso Gómez <valeryalfonso.ig@academia.umb.edu.co>
2026-08-04 22:49:48 -0500

    Delete BookNotFoundException.java

--- BookNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.umb.taller.exception;
-
-public class BookNotFoundException extends LibraryException {
-
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-
-}
